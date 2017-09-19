9e9994977444ebeb78a4c9e5ea85143c19561b81
Kirill Osenkov <github@osenkov.com>
2017-09-19 10:47:14 -0700

    Add a stub for Microsoft.VisualStudio.Text.Internal.dll.

---
+++ src/Microsoft.VisualStudio.Text.Internal/Language/Intellisense/ITelemetryDiagnosticID.cs
@@ -0,0 +1,21 @@
+﻿//
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License. See License.txt in the project root for license information.
+//
+// This file contain internal APIs that are subject to change without notice.
+// Use at your own risk.
+//
+namespace Microsoft.VisualStudio.Language.Intellisense
+{
+    /// <summary>
+    /// Represents an object that can provide a Diagnostic ID for telemetry purposes.
+    /// </summary>
+    public interface ITelemetryDiagnosticID<T>
+    {
+        /// <summary>
+        /// Get Diagnostic ID for telemetry purposes.
+        /// </summary>
+        /// <returns></returns>
+        T GetDiagnosticID();
+    }
+}
